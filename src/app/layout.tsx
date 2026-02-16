fd55dfe77cf760327af9df5c43670e479c5c6df7
Keksuccino <Keksuccino@users.noreply.github.com>
2026-02-16 08:04:26 +0100

    Make Material icons load reliably via local font package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import { Fira_Code, Manrope, Space_Grotesk } from "next/font/google";
 
 import { AppHeader } from "@/components/app-header";
 import { ThemeProvider } from "@/components/theme-provider";
 
+import "@fontsource/material-symbols-outlined";
 import "./globals.css";
 
 const fontDisplay = Space_Grotesk({
@@ -37,14 +37,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-color-mode="light">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@0..1&display=optional"
-        />
-      </head>
       <body className={`${fontDisplay.variable} ${fontBody.variable} ${fontMono.variable}`}>
         <ThemeProvider>
           <a href="#main-content" className="skip-link">
